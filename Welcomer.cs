0778e3fb843bbf8fcaeb6558e50a6cec300d05bd
Dylan <65882593+Phoenix557@users.noreply.github.com>
2022-07-09 17:34:46 -0400

    Add files via upload

---
+++ Welcomer.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+using Oxide.Game.Rust.Libraries;
+using Oxide.Core.Libraries.Covalence;
+using Oxide.Core.Plugins;
+
+
+namespace Oxide.Plugins
+{
+    [Info("Welcomer", "Phoenix", "1.0.0")]
+    [Description("Sends welcome/goodbye message when joining server")]
+    public class Welcomer : CovalencePlugin
+    {
+        #region void
+        void Init()
+        {
+            Puts("Loaded Welcomer successfully!");
+        }
+
+        #endregion
+
+        #region OnPlayerConnect&OnPlayerDisconnected
+        void OnPlayerConnected(BasePlayer player)
+        {
+            server.Broadcast($"<color=#5891ed>{player.displayName}</color> has washed ashore!");
+            Puts($"{player.displayName} has connected!");
+        }
+
+
+        /*
+            Called after the player has disconnected from the server
+            No return behavior
+        */
+        void OnPlayerDisconnected(BasePlayer player, string reason)
+        {
+            server.Broadcast($"<color=#5891ed>{player.displayName}</color> has been lost at sea!");
+            Puts($"{player.displayName} has disconnected!({reason})");
+        }
+        #endregion
+    }
+}
